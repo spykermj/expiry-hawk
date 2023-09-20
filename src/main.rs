34b16ae6cbc8f49eb2ce81abdf0534c17fec67de
Jeremy Spykerman <jeremy.spykerman@gmail.com>
2023-09-20 07:02:42 +0100

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use prometheus::{Counter, Encoder, Gauge, HistogramVec, TextEncoder, IntGaugeVec};
-use prometheus::{labels, opts, register_counter, register_gauge, register_histogram_vec, register_int_gauge_vec};
+use prometheus::{
+    labels, opts, register_counter, register_gauge, register_histogram_vec, register_int_gauge_vec,
+};
+use prometheus::{Counter, Encoder, Gauge, HistogramVec, IntGaugeVec, TextEncoder};
 
 use lazy_static::lazy_static;
 
@@ -20,13 +22,18 @@ use kube::{
 use tracing::*;
 
 lazy_static! {
-    static ref SECRET_ROTATION_TIME: IntGaugeVec =
-        register_int_gauge_vec!("expiry_hawk_secret_rotation_time", "The time this secret can be rotated", &["namespace", "name", "kind"]).unwrap();
-
-
-    static ref SECRET_EXPIRY_TIME: IntGaugeVec =
-        register_int_gauge_vec!("expiry_hawk_secret_expiry_time", "The time this secret expires", &["namespace", "name", "kind"]).unwrap();
-
+    static ref SECRET_ROTATION_TIME: IntGaugeVec = register_int_gauge_vec!(
+        "expiry_hawk_secret_rotation_time",
+        "The time this secret can be rotated",
+        &["namespace", "name", "kind"]
+    )
+    .unwrap();
+    static ref SECRET_EXPIRY_TIME: IntGaugeVec = register_int_gauge_vec!(
+        "expiry_hawk_secret_expiry_time",
+        "The time this secret expires",
+        &["namespace", "name", "kind"]
+    )
+    .unwrap();
     static ref HTTP_COUNTER: Counter = register_counter!(opts!(
         "expiry_hawk_http_requests_total",
         "Number of HTTP requests made.",
@@ -39,7 +46,6 @@ lazy_static! {
         labels! {"handler" => "metrics",}
     ))
     .unwrap();
-
     static ref HTTP_REQ_HISTOGRAM: HistogramVec = register_histogram_vec!(
         "expiry_hawk_http_request_duration_seconds",
         "The HTTP request latencies in seconds.",
@@ -52,7 +58,9 @@ async fn serve_req(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> 
     let encoder = TextEncoder::new();
 
     HTTP_COUNTER.inc();
-    let timer = HTTP_REQ_HISTOGRAM.with_label_values(&["metrics"]).start_timer();
+    let timer = HTTP_REQ_HISTOGRAM
+        .with_label_values(&["metrics"])
+        .start_timer();
 
     let metric_families = prometheus::gather();
     let mut buffer = vec![];
@@ -71,9 +79,9 @@ async fn serve_req(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> 
 }
 
 enum Kind {
-   Deployment,
-   StatefulSet,
-   DaemonSet,
+    Deployment,
+    StatefulSet,
+    DaemonSet,
 }
 
 fn get_rotation_annotation() -> String {
@@ -84,7 +92,7 @@ fn get_expiry_annotation() -> String {
     "spykerman.co.uk/secret-expiry-time".to_string()
 }
 
-async fn serve_metrics() -> anyhow::Result<()>{
+async fn serve_metrics() -> anyhow::Result<()> {
     let addr = ([0, 0, 0, 0], 9898).into();
     info!("Metrics server listening on http://{}", addr);
 
@@ -108,18 +116,22 @@ async fn main() {
 
 fn init_tracing() {
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
-    .with_max_level(Level::INFO)
-    .finish();
-    tracing::subscriber::set_global_default(subscriber)
-    .expect("setting default subscriber failed");
+        .with_max_level(Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 }
 
-async fn scan_and_serve() -> anyhow::Result<((),(),(),())> {
+async fn scan_and_serve() -> anyhow::Result<((), (), (), ())> {
     let serve_fut = serve_metrics();
     let watch_statefulset = watch_metadata(Kind::StatefulSet);
     let watch_deployment = watch_metadata(Kind::Deployment);
     let watch_daemonset = watch_metadata(Kind::DaemonSet);
-    try_join!(serve_fut, watch_deployment, watch_statefulset, watch_daemonset)
+    try_join!(
+        serve_fut,
+        watch_deployment,
+        watch_statefulset,
+        watch_daemonset
+    )
 }
 
 async fn watch_metadata(resource: Kind) -> anyhow::Result<()> {
@@ -145,31 +157,63 @@ async fn watch_metadata(resource: Kind) -> anyhow::Result<()> {
     handle_events(metadata_watcher(api, wc), &ar).await
 }
 
-async fn handle_events<
-    K: Resource<DynamicType = ApiResource> + Clone + Send + 'static,
->(
+async fn handle_events<K: Resource<DynamicType = ApiResource> + Clone + Send + 'static>(
     stream: impl Stream<Item = watcher::Result<Event<K>>> + Send + 'static,
     ar: &ApiResource,
 ) -> anyhow::Result<()> {
     let mut items = stream.applied_objects().boxed();
     while let Some(resource) = items.try_next().await? {
         if let Some(ns) = resource.namespace() {
-            if resource.annotations().contains_key(&get_rotation_annotation()) {
-                info!("parsing rotation time for {} {} in {ns}", K::kind(ar), resource.name_any());
-                let rfc3339 = resource.annotations().get(&get_rotation_annotation()).unwrap();
+            if resource
+                .annotations()
+                .contains_key(&get_rotation_annotation())
+            {
+                info!(
+                    "parsing rotation time for {} {} in {ns}",
+                    K::kind(ar),
+                    resource.name_any()
+                );
+                let rfc3339 = resource
+                    .annotations()
+                    .get(&get_rotation_annotation())
+                    .unwrap();
                 if let Ok(timestamp) = DateTime::parse_from_rfc3339(rfc3339) {
-                    SECRET_ROTATION_TIME.with_label_values(&[&ns, &resource.name_any(), &K::kind(ar)]).set(timestamp.timestamp_millis())
+                    SECRET_ROTATION_TIME
+                        .with_label_values(&[&ns, &resource.name_any(), &K::kind(ar)])
+                        .set(timestamp.timestamp_millis())
                 } else {
-                  error!("{} {} in {ns} failed to parse as rfc3339: {}", K::kind(ar), &resource.name_any(), rfc3339)
+                    error!(
+                        "{} {} in {ns} failed to parse as rfc3339: {}",
+                        K::kind(ar),
+                        &resource.name_any(),
+                        rfc3339
+                    )
                 }
             }
-            if resource.annotations().contains_key(&get_expiry_annotation()) {
-                info!("parsing expiry time for {} {} in {ns}", K::kind(ar), resource.name_any());
-                let rfc3339 = resource.annotations().get(&get_expiry_annotation()).unwrap();
+            if resource
+                .annotations()
+                .contains_key(&get_expiry_annotation())
+            {
+                info!(
+                    "parsing expiry time for {} {} in {ns}",
+                    K::kind(ar),
+                    resource.name_any()
+                );
+                let rfc3339 = resource
+                    .annotations()
+                    .get(&get_expiry_annotation())
+                    .unwrap();
                 if let Ok(timestamp) = DateTime::parse_from_rfc3339(rfc3339) {
-                    SECRET_EXPIRY_TIME.with_label_values(&[&ns, &resource.name_any(), &K::kind(ar)]).set(timestamp.timestamp_millis())
+                    SECRET_EXPIRY_TIME
+                        .with_label_values(&[&ns, &resource.name_any(), &K::kind(ar)])
+                        .set(timestamp.timestamp_millis())
                 } else {
-                  error!("{} {} in {ns} failed to parse as rfc3339: {}", K::kind(ar), &resource.name_any(), rfc3339)
+                    error!(
+                        "{} {} in {ns} failed to parse as rfc3339: {}",
+                        K::kind(ar),
+                        &resource.name_any(),
+                        rfc3339
+                    )
                 }
             }
         }
