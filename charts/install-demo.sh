58a45c613064b98f1ff3a3599616d82fc12d97d1
Jeremy Spykerman <jeremy.spykerman@gmail.com>
2023-09-24 07:22:14 +0100

    Randomly generate the expiry times
    The demo was not very interesting because everything was going to expire
    in 90 days.
    
    Now, expiry is automatically generated between 0 and 90 days for each
    monitored item.

--- charts/install-demo.sh
+++ charts/install-demo.sh
@@ -3,6 +3,6 @@
 
 for ns in one two three four five six seven eight nine ten; do
     for i in alpha bravo charlie delta echo; do
-        helm -n $ns install $i --create-namespace ./demo
+        helm -n $ns install $i --set expiryDays=`shuf -i 0-90 -n 1` --create-namespace ./demo
     done
 done
